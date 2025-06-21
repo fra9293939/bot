bc85dc84a97d79f7fb2c31d7563fdc8edec43b39
fra9293939 <stasi.fr4x14.outhz4@gmail.com>
2025-06-21 19:16:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import discord
-from discord.ext import commands, tasks
-import datetime
+from discord.ext import commands
 import os
-from zoneinfo import ZoneInfo
 from keep_alive import keep_alive
 
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-CHANNEL_ID = int(os.getenv("DISCORD_CHANNEL_ID"))
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -91,42 +88,6 @@ async def comandi(ctx):
 @bot.event
 async def on_ready():
     print(f"✅ Bot attivo come {bot.user}")
-    await bot.wait_until_ready()
-    buongiorno.start()
-    buonanotte.start()
-    compleanno.start()
-
-@tasks.loop(time=datetime.time(hour=8, minute=0, tzinfo=ZoneInfo("Europe/Rome")))
-async def buongiorno():
-    try:
-        canale = await bot.fetch_channel(CHANNEL_ID)
-        await canale.send("Buongiorno belli🥰!")
-    except Exception as e:
-        print(f"❌ Errore invio buongiorno: {e}")
-
-@tasks.loop(time=datetime.time(hour=0, minute=0, tzinfo=ZoneInfo("Europe/Rome")))
-async def buonanotte():
-    try:
-        canale = await bot.fetch_channel(CHANNEL_ID)
-        await canale.send("Buonanotte belli!🥰")
-    except Exception as e:
-        print(f"❌ Errore invio buonanotte: {e}")
-
-@tasks.loop(time=datetime.time(hour=0, minute=0, tzinfo=ZoneInfo("Europe/Rome")))
-async def compleanno():
-    now = datetime.datetime.now(ZoneInfo("Europe/Rome"))
-    print(f"Controllo compleanno: {now}")
-    if now.month == 6 and now.day == 20:
-        try:
-            canale = await bot.fetch_channel(CHANNEL_ID)
-            await canale.send("@everyone È IL COMPLEANNO DI <@734059577078972566> RIEMPITE IL SERVER CON MESSAGGI DI AUGURI! 🥰")
-            compleanno.cancel()
-        except Exception as e:
-            print(f"Errore invio compleanno: {e}")
-
-@compleanno.before_loop
-async def before_compleanno():
-    await bot.wait_until_ready()
 
 keep_alive()
 bot.run(TOKEN)
