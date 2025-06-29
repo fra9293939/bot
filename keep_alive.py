624ce15104dbf86fb5d29e168210208102d9d2f2
fra9293939 <stasi.fr4x14.outhz4@gmail.com>
2025-06-29 19:16:20 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -9,7 +9,7 @@ def home():
     return "Bot attivo ✅"
 
 def run():
-    port = int(os.environ.get("PORT", 8080))  # Prende la porta da Render o usa 8080 di default
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
