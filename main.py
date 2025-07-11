5f378895a947bac15a0dc180dcd6691c02ae3d01
fra9293939 <stasi.fr4x14.outhz4@gmail.com>
2025-07-11 16:53:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ async def discord_cmd(ctx):
 async def orari(ctx):
     embed = discord.Embed(
         title="📅 Streaming Schedule",
-        description="TUTTI I GIORNI DALLE 18:00 ALLE 21:00🕑 (salvo imprevisti, vi avvisiamo su ig e ds)",
+        description="TUTTI I GIORNI DALLE 20:45 ALLE 23:45🕑 (salvo imprevisti, vi avvisiamo su ig e ds)",
         color=0xB500FF
     )
     await ctx.send(embed=embed)
